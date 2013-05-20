8045d94bc42e8a65d6fff9aa1fbfa2fd12fba73b
Sean Walbran <swalbran@socialcast.com>
2013-05-20 14:29:41 -0500

    clean up spec_helper and fix apn config for specs

--- spec/apn_on_rails/app/models/apn/notification_spec.rb
+++ spec/apn_on_rails/app/models/apn/notification_spec.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-require File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'spec_helper.rb')
+require 'spec_helper'
 
 describe APN::Notification do
 
--- spec/apn_on_rails/libs/connection_spec.rb
+++ spec/apn_on_rails/libs/connection_spec.rb
@@ -1,9 +1,9 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require 'spec_helper'
 
 describe APN::Connection do
 
   describe 'open_for_delivery' do
-    
+
     it 'should create a connection to Apple, yield it, and then close' do
       rsa_mock = mock('rsa_mock')
       OpenSSL::PKey::RSA.should_receive(:new).with(apn_cert, '').and_return(rsa_mock)
@@ -15,11 +15,11 @@ describe APN::Connection do
       ctx_mock.should_receive(:key=).with(rsa_mock)
       ctx_mock.should_receive(:cert=).with(cert_mock)
       OpenSSL::SSL::SSLContext.should_receive(:new).and_return(ctx_mock)
-      
+
       tcp_mock = mock('tcp_mock')
       tcp_mock.should_receive(:close)
       TCPSocket.should_receive(:new).with('gateway.sandbox.push.apple.com', 2195).and_return(tcp_mock)
-      
+
       ssl_mock = mock('ssl_mock')
       ssl_mock.should_receive(:sync=).with(true)
       ssl_mock.should_receive(:connect)
@@ -27,14 +27,14 @@ describe APN::Connection do
       ssl_mock.should_receive(:write).with('message-1')
       ssl_mock.should_receive(:close)
       OpenSSL::SSL::SSLSocket.should_receive(:new).with(tcp_mock, ctx_mock).and_return(ssl_mock)
-      
+
       APN::Connection.open_for_delivery do |conn, sock|
         conn.write('message-0')
         conn.write('message-1')
       end
-      
+
     end
-    
+
   end
-  
+
 end
